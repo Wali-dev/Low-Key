080c79a902e16e939a941da7a237d2a6264156cb
Wali Cornell <cornelhassan780@gmail.com>
2024-08-22 20:29:56 +0530

    homepage added

--- src/app/(auth)/verify/[username]/page.tsx
+++ src/app/(auth)/verify/[username]/page.tsx
@@ -33,7 +33,7 @@ const Page = () => {
                 title: "Success",
                 description: response.data.message
             })
-            router.replace('signin')
+            router.replace('/signin')
         } catch (error) {
             console.error("error verifying user ", error)
             const axiosError = error as AxiosError<ApiResponse>
--- src/app/auth/[...nextauth]/options.ts
+++ src/app/auth/[...nextauth]/options.ts
@@ -1,3 +1,76 @@
+// import { NextAuthOptions } from 'next-auth';
+// import CredentialsProvider from 'next-auth/providers/credentials';
+// import bcrypt from 'bcryptjs';
+// import dbConnect from '@/lib/dbConnect';
+// import UserModel from '@/model/User';
+
+// export const authOptions: NextAuthOptions = {
+//     providers: [
+//         CredentialsProvider({
+//             id: 'credentials',
+//             name: 'Credentials',
+//             credentials: {
+//                 email: { label: 'Email', type: 'text' },
+//                 password: { label: 'Password', type: 'password' },
+//             },
+//             async authorize(credentials: any): Promise<any> {
+//                 await dbConnect();
+//                 try {
+//                     const user = await UserModel.findOne({
+//                         $or: [
+//                             { email: credentials.identifier },
+//                             { username: credentials.identifier },
+//                         ],
+//                     });
+//                     if (!user) {
+//                         throw new Error('no user found with this email');
+//                     }
+//                     if (!user.isVerified) {
+//                         throw new Error('please verify your account before logging in');
+//                     }
+//                     const isPasswordCorrect = await bcrypt.compare(
+//                         credentials.password,
+//                         user.password
+//                     );
+//                     if (isPasswordCorrect) {
+//                         return user;
+//                     } else {
+//                         throw new Error('incorrect password');
+//                     }
+//                 } catch (err: any) {
+//                     throw new Error(err);
+//                 }
+//             },
+//         }),
+//     ],
+//     callbacks: {
+//         async jwt({ token, user }) {
+//             if (user) {
+//                 token._id = user._id?.toString(); // Convert ObjectId to string
+//                 token.isVerified = user.isVerified;
+//                 token.isAcceptingMessages = user.isAcceptingMessages;
+//                 token.username = user.username;
+//             }
+//             return token;
+//         },
+//         async session({ session, token }) {
+//             if (token) {
+//                 session.user._id = token._id;
+//                 session.user.isVerified = token.isVerified;
+//                 session.user.isAcceptingMessages = token.isAcceptingMessages;
+//                 session.user.username = token.username;
+//             }
+//             return session;
+//         },
+//     },
+//     session: {
+//         strategy: 'jwt',
+//     },
+//     secret: process.env.NEXTAUTH_SECRET,
+//     pages: {
+//         signIn: '/sign-in',
+//     },
+// };
 import { NextAuthOptions } from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import bcrypt from 'bcryptjs';
@@ -23,10 +96,10 @@ export const authOptions: NextAuthOptions = {
                         ],
                     });
                     if (!user) {
-                        throw new Error('no user found with this email');
+                        throw new Error('No user found with this email');
                     }
                     if (!user.isVerified) {
-                        throw new Error('please verify your account before logging in');
+                        throw new Error('Please verify your account before logging in');
                     }
                     const isPasswordCorrect = await bcrypt.compare(
                         credentials.password,
@@ -35,9 +108,10 @@ export const authOptions: NextAuthOptions = {
                     if (isPasswordCorrect) {
                         return user;
                     } else {
-                        throw new Error('incorrect password');
+                        throw new Error('Incorrect password');
                     }
                 } catch (err: any) {
+                    console.log(err)
                     throw new Error(err);
                 }
             },
@@ -68,6 +142,6 @@ export const authOptions: NextAuthOptions = {
     },
     secret: process.env.NEXTAUTH_SECRET,
     pages: {
-        signIn: '/sign-in',
+        signIn: '/signin',
     },
 };
